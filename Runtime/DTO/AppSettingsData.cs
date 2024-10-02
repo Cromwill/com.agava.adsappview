5faf62c34c5834d9f16927262a7d7b44b4a16ce6
Andrey Sucheninov <78255120+doCent90@users.noreply.github.com>
2024-10-02 11:46:39 +0300

    init

---
+++ Runtime/DTO/AppSettingsData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Scripting;
+
+namespace AdsAppView.DTO
+{
+    [Preserve]
+    public class AppSettingsData
+    {
+        public string app_id {  get; set; }
+        public string store_id {  get; set; }
+        public string platform { get; set; }
+        public string ads_app_id {  get; set; }
+        public int first_timer {  get; set; }
+        public int regular_timer {  get; set; }
+        public bool carousel {  get; set; }
+        public int carousel_count {  get; set; }
+    }
+}
---
+++ Runtime/Program/ViewPresenter.cs
@@ -0,0 +1,58 @@
+﻿using AdsAppView.DTO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AdsAppView.Utility
+{
+    public class ViewPresenter : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _windowCanvasGrp;
+        [SerializeField] private Image _image;
+        [SerializeField] private Button _linkBtn;
+        [SerializeField] private Button _closeBtn;
+
+        private Links _links;
+        private string _link;
+
+        public void Construct(Links links)
+        {
+            _links = links;
+            _linkBtn.onClick.AddListener(OnLinkClicked);
+            _closeBtn.onClick.AddListener(OnCloseClicked);
+            DisableCanvasGroup(_windowCanvasGrp);
+        }
+
+        public void Show(SpriteData sprite)
+        {
+            _image.sprite = sprite.sprite;
+            _link = sprite.link;
+            EnableCanvasGroup(_windowCanvasGrp);
+        }
+
+        private void OnLinkClicked()
+        {
+            Debug.LogFormat($"Open link {_link}");
+
+            if (string.IsNullOrEmpty(_link))
+                return;
+
+            Application.OpenURL(_link);
+        }
+
+        private void OnCloseClicked() => DisableCanvasGroup(_windowCanvasGrp);
+
+        private void EnableCanvasGroup(CanvasGroup canvas)
+        {
+            canvas.alpha = 1;
+            canvas.interactable = true;
+            canvas.blocksRaycasts = true;
+        }
+
+        private void DisableCanvasGroup(CanvasGroup canvas)
+        {
+            canvas.alpha = 0;
+            canvas.interactable = false;
+            canvas.blocksRaycasts = false;
+        }
+    }
+}
