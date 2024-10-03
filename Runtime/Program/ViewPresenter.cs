8b1c15c83097a273171e0a29067b059e395ff652
marriuss <53435883+marriuss@users.noreply.github.com>
2024-10-03 13:23:59 +0400

    Add caching; fixes

--- Runtime/Program/ViewPresenter.cs
+++ Runtime/Program/ViewPresenter.cs
@@ -1,4 +1,5 @@
-﻿using AdsAppView.DTO;
+﻿using System;
+using AdsAppView.DTO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,13 @@ namespace AdsAppView.Utility
         [SerializeField] private CanvasGroup _windowCanvasGrp;
         [SerializeField] private Image _image;
         [SerializeField] private Button _linkBtn;
+        [SerializeField] private AspectRatioFitter _aspectRatioFitter;
         [SerializeField] private Button _closeBtn;
 
         private Links _links;
         private string _link;
+
+        public bool Enabled { get; private set; } = false;
 
         public void Construct(Links links)
         {
@@ -25,13 +29,16 @@ namespace AdsAppView.Utility
         public void Show(SpriteData sprite)
         {
             _image.sprite = sprite.sprite;
+            _aspectRatioFitter.aspectRatio = sprite.aspectRatio;
             _link = sprite.link;
             EnableCanvasGroup(_windowCanvasGrp);
         }
 
         private void OnLinkClicked()
         {
+#if UNITY_EDITOR
             Debug.LogFormat($"Open link {_link}");
+#endif
 
             if (string.IsNullOrEmpty(_link))
                 return;
@@ -46,6 +53,7 @@ namespace AdsAppView.Utility
             canvas.alpha = 1;
             canvas.interactable = true;
             canvas.blocksRaycasts = true;
+            Enabled = true;
         }
 
         private void DisableCanvasGroup(CanvasGroup canvas)
@@ -53,6 +61,7 @@ namespace AdsAppView.Utility
             canvas.alpha = 0;
             canvas.interactable = false;
             canvas.blocksRaycasts = false;
+            Enabled = false;
         }
     }
 }
