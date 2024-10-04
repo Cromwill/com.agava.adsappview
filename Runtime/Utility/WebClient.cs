1489df91275ca08deae381dac61322afef421b80
Andrey Sucheninov <78255120+doCent90@users.noreply.github.com>
2024-10-04 20:26:54 +0300

    Fix get settings request

--- Runtime/Utility/WebClient.cs
+++ Runtime/Utility/WebClient.cs
@@ -45,6 +45,22 @@ namespace AdsAppView.Utility
         {
             string path = $"{GetHttpPath(request.api_name)}";
 
+            using (UnityWebRequest webRequest = CreateWebRequest(path, RequestType.GET, uploadBody: request.body))
+            {
+                webRequest.SendWebRequest();
+
+                await WaitProccessing(webRequest);
+                TryShowRequestInfo(webRequest, request.api_name);
+
+                var body = webRequest.downloadHandler.text;
+                return new Response(webRequest.result, webRequest.result.ToString(), body, false, null);
+            }
+        }
+
+        public async Task<Response> GetAppSettings(Request request)
+        {
+            string path = $"{GetHttpPath(request.api_name)}";
+
             using (UnityWebRequest webRequest = CreateWebRequest(path, RequestType.POST, uploadBody: request.body))
             {
                 webRequest.SendWebRequest();
